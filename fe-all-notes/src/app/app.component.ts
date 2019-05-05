daa2e355ddea3e78bc936ded0b3d53511cc40887
Piotr Dyszkowski <piod94@gmail.com>
2019-05-05 13:31:41 +0200

    Add angular template

---
+++ fe-all-notes/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dk-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'fe-all-notes';
+}
