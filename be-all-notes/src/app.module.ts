5613495d0ab495c67fb4dc713678928d68e32f66
Piotr Dyszkowski <piod94@gmail.com>
2019-08-18 17:15:27 +0200

    Support for SPA

--- be-all-notes/src/app.module.ts
+++ be-all-notes/src/app.module.ts
@@ -1,13 +1,23 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 // TODO SSL connection do db
 
 @Module({
-  imports: [NotesModule, TypeOrmModule.forRoot()],
+  imports: [
+    NotesModule,
+    TypeOrmModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'dist/fe-all-notes'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
